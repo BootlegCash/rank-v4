2ea88f02949ec4326cf4fd7de47b13ad85e802ba
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-15 10:34:31 -0700

    Update api_urls.py

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -1,12 +1,15 @@
 # accounts/api_urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api import DailyLogViewSet  # Make sure this exists and imports correctly
+from .api import DailyLogViewSet, user_profile
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'log_drink', DailyLogViewSet, basename='log-drink')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', user_profile, name='api_profile'),
 ]
