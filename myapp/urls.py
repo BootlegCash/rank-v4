5e91c3da56a4e24e382a369df569222ef0ee8fce
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-15 02:50:57 -0700

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path, include
+# myapp/urls.py
+
 from django.contrib import admin
-from django.views.generic import RedirectView  # Add this import
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', RedirectView.as_view(url='accounts/welcome/')),
+    path('accounts/', include('accounts.urls')),  # Main app routes
+    path('api/', include('accounts.api_urls')),   # DRF API routes
+    path('', RedirectView.as_view(url='/accounts/welcome/', permanent=False)),  # Default redirect
 ]
