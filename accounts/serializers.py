0ab44100c40ab4e787757ae1986de94bbfdd7b90
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-16 14:41:31 -0700

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,33 @@
 # accounts/serializers.py
+
 from rest_framework import serializers
 from .models import DailyLog
 
 class DailyLogSerializer(serializers.ModelSerializer):
+    beer = serializers.IntegerField(required=False, default=0)
+    floco = serializers.IntegerField(required=False, default=0)
+    rum = serializers.IntegerField(required=False, default=0)
+    whiskey = serializers.IntegerField(required=False, default=0)
+    vodka = serializers.IntegerField(required=False, default=0)
+    tequila = serializers.IntegerField(required=False, default=0)
+    shotguns = serializers.IntegerField(required=False, default=0)
+    snorkels = serializers.IntegerField(required=False, default=0)
+    thrown_up = serializers.BooleanField(required=False, default=False)
+    xp = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = DailyLog
         fields = [
-            'date', 'profile', 'beer', 'floco', 'rum', 'whiskey',
-            'vodka', 'tequila', 'shotguns', 'snorkels', 'thrown_up', 'xp'
+            'date',
+            'beer',
+            'floco',
+            'rum',
+            'whiskey',
+            'vodka',
+            'tequila',
+            'shotguns',
+            'snorkels',
+            'thrown_up',
+            'xp'
         ]
-        read_only_fields = ['date', 'profile', 'xp']
+        read_only_fields = ['date', 'xp']
