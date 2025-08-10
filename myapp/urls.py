ff95402cd20a93e3850dc6cf1d331ff971fd45f8
ryan <aolson949@gmail.com>
2025-08-10 11:16:31 -0700

    fixed stupid shit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import views as accounts_views
 from rest_framework.authtoken import views as drf_views
+from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import RedirectView
+
+
 
 urlpatterns = [
 
@@ -12,7 +16,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 
     # ✅ Mount API endpoints at /accounts/api/
-    path('accounts/api/', include('accounts.api_urls')),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 
     path('api/token/', drf_views.obtain_auth_token, name='api_token_auth'),
 ]
