ff236c5c768cc2cef91934024322648a57fb5cb7
Ryan Olson <ryanolson@mac.mynetworksettings.com>
2026-02-24 02:04:24 -0700

    fixed email bs

--- accounts/urls.py
+++ accounts/urls.py
@@ -33,24 +33,11 @@ urlpatterns = [
 
     # API namespace for mobile
     path('api/', include('accounts.api_urls')),
+# Password reset flow (neon themed)
+    path("password_reset/", views.NeonPasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", views.NeonPasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", views.NeonPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", views.NeonPasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-    template_name='accounts/password_reset_form.html',
-    email_template_name='accounts/password_reset_email.txt',
-    subject_template_name='accounts/password_reset_subject.txt',
-), name='password_reset'),
-
-path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-    template_name='accounts/password_reset_done.html'
-), name='password_reset_done'),
-
-path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-    template_name='accounts/password_reset_confirm.html'
-), name='password_reset_confirm'),
-
-path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-    template_name='accounts/password_reset_complete.html'
-), name='password_reset_complete'),
 ]
-
 
